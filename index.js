9c06c1e24aaf948f654a6a8a8c68b4335ae5c4bc
Danylo Zymbalevskyi <zymbalevskyy@gmail.com>
2023-06-11 16:18:01 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+let dodger;
+function moveDodgerLeft() {
+  let currentPosition = parseInt(dodger.style.left) || 0;
+  let newPosition = currentPosition - 1;
+  dodger.style.left = `${newPosition}px`;
+}
+
+function moveDodgerRight() {
+  let currentPosition = parseInt(dodger.style.left) || 0;
+  let newPosition = currentPosition + 1;
+  dodger.style.left = `${newPosition}px`;
+}
